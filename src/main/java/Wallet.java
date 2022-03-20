baddb56b87d5dcdfa1497e4513fc139fa52529ea
Sheetal Ramling <sheetalramling@Sheetals-MacBook-Pro.local>
2022-03-20 21:58:55 +0530

    Add. Functionality to add money in multiple currencies and return sum in preferred currency

--- src/main/java/Wallet.java
+++ src/main/java/Wallet.java
@@ -1,10 +1,42 @@
+import java.util.Currency;
+
 public class Wallet {
     Money money;
-    static double minBalance = 100;
+    static double balance = 0;
+    Money[] moneyInMultipleCurrencies;
+
 
     public Wallet(Money money) {
         this.money = money;
+
     }
+
+    public Wallet(Money[] moneyInMultipleCurrencies) {
+        this.moneyInMultipleCurrencies = moneyInMultipleCurrencies;
+    }
+
+    public double checkBalance(Money[] moneyInMultipleCurrencies, Currency preferredCurrency) {
+        double moneyInPreferredCurrency = 0, moneyInOtherCurrency = 0;
+        for (Money money : moneyInMultipleCurrencies) {
+
+            if (money.currency.getCurrencyCode().equals(preferredCurrency.getCurrencyCode())) {
+                moneyInPreferredCurrency = money.amount;
+
+            } else {
+                if (money.currency.getCurrencyCode().equals("USD")) {
+                    moneyInOtherCurrency = money.DollarToRupeeConverter();
+                }
+                if (money.currency.getCurrencyCode().equals("INR")) {
+                    moneyInOtherCurrency = money.RupeeToDollarConverter();
+                }
+            }
+
+            moneyInPreferredCurrency = moneyInPreferredCurrency + moneyInOtherCurrency;
+        }
+
+        return moneyInPreferredCurrency;
+    }
+
 
     @Override
     public boolean equals(Object o) {
@@ -13,26 +45,22 @@ public class Wallet {
 
         Wallet that = (Wallet) o;
 
-        if (money.currency.getCurrencyCode().equals("INR"))
-            money.amount = money.RupeeToDollarConverter();
-        if (money.currency.getCurrencyCode().equals("USD"))
-            money.amount = money.DollarToRupeeConverter();
+        if (money.currency.getCurrencyCode().equals("INR")) money.amount = money.RupeeToDollarConverter();
+        if (money.currency.getCurrencyCode().equals("USD")) money.amount = money.DollarToRupeeConverter();
 
         return money.amount == that.money.amount;
 
     }
 
-    public double creditAmountToWallet(){
-        if(money.amount>0)
-        minBalance = minBalance +money.amount;
-        return minBalance;
+    public double creditAmountToWallet() {
+        if (money.amount > 0) balance = balance + money.amount;
+        return balance;
     }
 
     public double debitAmountFromWallet() throws InsufficientBalanceException {
-        if(minBalance ==0.0)
-            throw new InsufficientBalanceException("No Sufficient Balance");
-        minBalance = minBalance -money.amount;
-        return minBalance;
+        if (balance == 0.0) throw new InsufficientBalanceException("No Sufficient Balance");
+        balance = balance - money.amount;
+        return balance;
     }
 
 }
--- src/test/java/WalletTest.java
+++ src/test/java/WalletTest.java
@@ -1,9 +1,10 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.util.Currency;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.equalTo;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WalletTest {
     @Test
@@ -33,7 +34,7 @@ public class WalletTest {
 
         double new_balance = moneyToBeCredited.creditAmountToWallet();
 
-        assertThat(Wallet.minBalance, is(equalTo(new_balance)));
+        assertThat(Wallet.balance, is(equalTo(new_balance)));
 
     }
 
@@ -45,8 +46,41 @@ public class WalletTest {
 
         double new_balance = moneyToBeDebited.debitAmountFromWallet();
 
-        assertThat(Wallet.minBalance, is(equalTo(new_balance)));
+        assertThat(Wallet.balance, is(equalTo(new_balance)));
+
+
+
+
+    }
+//    @Test
+//    void ShouldThrowInsufficientException() throws InsufficientBalanceException {
+//        Money thirtyFiveRupees = new Money(35, Currency.getInstance("INR"));
+//        Wallet moneyToBeDebited = new Wallet(thirtyFiveRupees);
+//
+//        assertThrows(InsufficientBalanceException.class, moneyToBeDebited::debitAmountFromWallet);
+//
+//    }
+
+
+    @Test
+    void ShouldDisplayAmountInPreferredCurrency() {
+        Money[] moneyInDifferentCurrencies={new Money(50,Currency.getInstance("INR")),new Money(1,Currency.getInstance("USD"))};
+        Wallet multipleCurrencies=new Wallet(moneyInDifferentCurrencies) ;
+
+        double totalAmount=multipleCurrencies.checkBalance(moneyInDifferentCurrencies,Currency.getInstance("INR"));
+
+        assertEquals(124.85,totalAmount);
 
     }
 
+    @Test
+    void ShouldDisplayAmountInPreferredCurrency1() {
+        Money[] moneyInDifferentCurrencies={new Money(74.85,Currency.getInstance("INR")),new Money(1,Currency.getInstance("USD")), new Money(149.7,Currency.getInstance("INR"))};
+        Wallet multipleCurrencies=new Wallet(moneyInDifferentCurrencies) ;
+
+        double totalAmount=multipleCurrencies.checkBalance(moneyInDifferentCurrencies,Currency.getInstance("USD"));
+
+        assertEquals(4,totalAmount);
+
+    }
 }
